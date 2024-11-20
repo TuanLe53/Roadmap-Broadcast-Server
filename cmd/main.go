c0932b8374331a78105bad8088a47f7bf4738f92
Tuan Le <leanhtuan030523@gmail.com>
2024-11-20 20:08:06 +0700

    Add main.go file

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/TuanLe53/Roadmap-Broadcast-Server/handlers"
+	"github.com/TuanLe53/Roadmap-Broadcast-Server/pkg/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func serverWS(pool *websocket.Pool, c echo.Context) error {
+	conn, err := websocket.Upgrade(c)
+	if err != nil {
+		log.Println(err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+
+	return nil
+}
+
+func main() {
+	app := echo.New()
+
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+
+	homeHandler := handlers.HomeHandler{}
+	app.GET("/", homeHandler.HandleHome)
+
+	app.GET("/ws", func(c echo.Context) error {
+		return serverWS(pool, c)
+	})
+
+	app.Logger.Fatal(app.Start(":5050"))
+}
